fb17647968bbe097ac980bedace84bb88778cbbb
hxwei88 <45229654+hxwei88@users.noreply.github.com>
2018-11-27 18:57:02 +0800

    Test
    test

--- bingo/module/Application/src/Model/IndexModel.php
+++ bingo/module/Application/src/Model/IndexModel.php
@@ -30,6 +30,53 @@
 	    	$data = $adapter->query($query);
 	    	return $data->fetchAll();
 	    }
+
+	    public function login($username, $password){
+	    	$configArray = array(
+			    'driver' => 'Mysqli',
+			    'database' => 'game',
+			    'username' => 'gameOwner@phpgameserver',
+			    'password' => 'Password123',
+			    'hostname' => 'phpgameserver.mysql.database.azure.com',
+			    'port' => '3306'
+			 );
+
+	    	$adapter = new \Zend\Db\Adapter\Adapter($configArray);
+
+	    	$query = sprintf("CALL spRegister(`%s`, `%s`)", $username, $password);
+	    	
+	    	$data = $adapter->query($query);
+	    	return $data->fetchAll();
+	    }
+
+	    public function changePassword($oldPassword, $newPassword){
+	    	$configArray = array(
+			    'driver' => 'Mysqli',
+			    'database' => 'game',
+			    'username' => 'gameOwner@phpgameserver',
+			    'password' => 'Password123',
+			    'hostname' => 'phpgameserver.mysql.database.azure.com',
+			    'port' => '3306'
+			 );
+
+	    	$adapter = new \Zend\Db\Adapter\Adapter($configArray);
+
+	    	$query = sprintf("CALL spChangePassword(`%s`, `%s`)", $oldPassword, $newPassword);
+
+	    	$data = $adapter->query($query);
+	    	return $data->fetchAll();
+	    }
+
+	    public function showInfo($username){
+	    	$configArray = array(
+			    'driver' => 'Mysqli',
+			    'database' => 'game',
+			    'username' => 'gameOwner@phpgameserver',
+			    'password' => 'Password123',
+			    'hostname' => 'phpgameserver.mysql.database.azure.com',
+			    'port' => '3306'
+			 );
+	    }
 	}
 
 ?>
